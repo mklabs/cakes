35a463e349e9b45e5b5eee3d2d8fe26e2398a2ff
Mickael Daniel <daniel.mickael@gmail.com>
2011-09-12 23:14:14 +0200

    macro does the entire integration with build script, clone/pull, build, test
    cloning the repo if not done yet, making the build and running the test
    
    todo: break this up in another macro

--- h5bp/tests/helpers/macros.js
+++ h5bp/tests/helpers/macros.js
@@ -1,7 +1,10 @@
 var zombie = require('zombie'),
     colors = require('colors'),
     connect = require('connect'),
-    path = require('path');
+    path = require('path'),
+    child = require('child_process'),
+    exec = require('child_process').exec,
+    EventEmitter = require('events').EventEmitter;
 
 // ## macros
 //
@@ -24,16 +27,83 @@ var extendContext = function extendContext (context, vows) {
   return context;
 };
 
+var spawn = function spawn(cmd, args, callback) {
+  var stderr = [], stdout = [],
+      ch = child.spawn(cmd, args);
+
+  ch.stdout.pipe(process.stdout, {end: false});
+  ch.stderr.pipe(process.stderr);
+  ch.stdout.on('data', function(data) { stdout[stdout.length] = data; });
+  ch.stderr.on('data', function(data) { stderr[stderr.length] = data; });
+  ch.on('exit', (callback || function (code) {
+    ch.emit('end', code, stdout.join('\n'), stderr.join('\n'));
+  }));
+
+  return ch;
+};
+
 
 // **assertListen**: macro to start and listen to the port provided.
 // Starts up a new connect server with very basic middleware setup,
 // logger and static configured to serve the publish dir
+//
+// Now, does the entire integration with build script,
+// cloning the repo if not done yet, making the build
+// and runing the test
+//
+// todo: break this up in another macro
 macros.assertListen = function (port, root, vows) {
   var context = {
     topic: function () {
-      var server = connect.createServer()
-        .use(connect.static(path.join(__dirname, '../..', root)))
-        .listen(port, this.callback.bind(this))
+      var em = new EventEmitter,
+          build, server, stderr = [];
+
+      root = path.join(__dirname, '../..', root);
+
+      server = connect.createServer()
+        .use(connect.static(root));
+
+      // prior to the whole test suite to run, ensure the root dir is there
+      // otherwise, call the main build task and continue
+      var ok = path.existsSync(root);
+      if(ok) {
+        server.listen(port, em.emit.bind(em, 'success'));
+        return em;
+      }
+
+      console.log(('  » ' + root + ' needs to be there, going to execute cake build...').grey);
+
+      process.chdir(path.resolve(__dirname, '../../'));
+
+      var build = spawn('cake', ['build']);
+
+      build.on('end', function (code, stdout, stderr) {
+        var createproject = /createproject/.test(stderr);
+        if(code > 0 && !createproject) throw new Error(('cake build exited with code ' + code + '\n ' + stderr).red);
+
+        // if there's no error, continue to vows at this point
+        if(!createproject) return server.listen(port, em.emit.bind(em, 'success'));
+
+        // otherwise, run the createproject task without prompt and rerun the build
+
+        // grab the dir.source value from the error output
+        var output = stderr.match(/\/[^\s]+/)[0].split('/').reverse()[0];
+
+        // end run the cake project task
+        spawn('cake', ['-o', output, 'createproject'])
+          .on('end', function(code, stdout, stderr) {
+            // finally, we can go on with build and test
+
+            // rerun the build, this time should be all good
+            spawn('cake', ['build'])
+              .on('end', function(code, stdout, stderr) {
+                if (code > 0) throw new Error(stderr || stderr);
+                server.listen(port, em.emit.bind(em, 'success'));
+              });
+          });
+      });
+
+      return em;
     }
   };
 
