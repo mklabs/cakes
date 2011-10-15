eea6c32347b7c7368ae9fc6f347eed841b214069
Mickael Daniel <daniel.mickael@gmail.com>
2011-10-15 17:33:24 +0200

    cakedebug script: run `open localhost:8080` directly

--- h5bp/cakedebug.js
+++ h5bp/cakedebug.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var fs   = require('fs');
+var exec = require('child_process').exec;
 var lib  = path.join(path.dirname(fs.realpathSync(__filename)), '../lib');
 
 // Use this script while node-inspector is running.
@@ -8,15 +9,14 @@ var lib  = path.join(path.dirname(fs.realpathSync(__filename)), '../lib');
 //
 //    node cakedebug.js --debug [task]
 //
-// Set a `debugger` statement and make sure to reload node-inspector
-// whitin the delay below. This will allow you to debug the script,
+// Set a `debugger` statement and make sure to have node-inspector
+// running in background. This will allow you to debug the script,
 // pretty neat.
-//
 
-var delay = 5000;
-console.log('\nHurry up, ', delay / 1000, ' seconds to debug out.');
 
-setTimeout(function() {
+// mac only: tweak this to match your environment
+exec('open http://localhost:8080', function(err) {
+  if(err) throw err;
   require('coffee-script/lib/cake').run();
-}, delay);
+});
 
